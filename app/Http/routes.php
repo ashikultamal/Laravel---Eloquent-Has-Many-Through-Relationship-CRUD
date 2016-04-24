57dc9f3e4a278d6515ed5227db3f80caadbe348c
tamal3209 <ashikul.tamal.aust@gmail.com>
2016-04-24 21:45:55 +0600

    Polymorphic init

---
+++ app/Http/routes.php
@@ -0,0 +1,69 @@
+<?php
+
+use App\Staff;
+use App\Photo;
+use App\Product;
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/create', function(){
+
+    $staff = Staff::find(1);
+
+    $staff->photos()->create(['path'=>'example.jpg']);
+
+});
+
+Route::get('/read', function(){
+
+    $staff = Staff::findOrFail(1);
+
+    foreach($staff->photos as $photo){
+        return $photo->path;
+    }
+
+
+});
+
+Route::get('/update', function(){
+    $staff = Staff::findOrFail(1);
+
+    $photo = $staff->photos()->whereId(1)->first();
+
+    $photo->path = "Update.jpg";
+
+    $photo->save();
+
+});
+
+Route::get('/delete', function(){
+
+    $staff = Staff::findOrFail(1);
+
+    $staff->photos()->whereId(1)->delete();
+});
+
+Route::get('/assign', function(){
+    $staff = Staff::findOrFail(1);
+    $photo = Photo::findOrFail(2);
+
+    $staff->photos()->save($photo);
+});
+
+Route::get('/unassign', function(){
+    $staff = Staff::findOrFail(1);
+
+    $staff->photos()->whereId(2)->update(['imageable_id'=>'', 'imageable_type'=>'']);
+});
